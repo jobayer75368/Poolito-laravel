49ff9141857cbbdc1dc3d31cfb26819c4b83fc60
Jobayer75368 <jobayer30325@gmail.com>
2026-07-15 00:11:47 +0600

    service model created

---
+++ database/migrations/2026_07_14_175759_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('service_name')->unique();
+            $table->string('service_slug')->unique();
+            $table->string('service_image');
+            $table->string('service_icon')->nullable();
+            $table->text('short_description');
+            $table->longText('long_description')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('frontend.index');
 });
+
+Route::get('/', [HomeController::class, 'index', function () {}]);
+
 Route::get('/about', function () {
     return view('frontend.about');
 });
